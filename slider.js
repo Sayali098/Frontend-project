b17a585e67d0348b34e007861b3c1fa2b318caa4
sayali shelake <shelakesayali9022@gmail.com>
2023-08-29 18:33:09 +0530

    project added to git

---
+++ slider.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 5000, // Set the delay in milliseconds (5 seconds in this example)
+      disableOnInteraction: false, // Continue autoplay even when user interacts with the swiper
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
